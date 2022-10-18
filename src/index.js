0acfe529befa5bb58eb7faa699e348ef68f17fdf
b45i <amalshajan2011@gmail.com>
2022-10-18 07:24:03 +0530

    zoom fix

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './style';
 import App from './app';
 
 export default App;
+
+function preventZoom(e) {
+    e.preventDefault();
+    document.body.style.zoom = 0.99;
+}
+
+document.addEventListener('gesturestart', preventZoom);
+document.addEventListener('gesturechange', preventZoom);
+document.addEventListener('gestureend', preventZoom);
