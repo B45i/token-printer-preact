2184de4b7419f543d7596023a23d61a346f1d038
b45i <amalshajan2011@gmail.com>
2022-10-18 06:48:22 +0530

    settings list

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
 import Navbar from './components/Navbar';
+import SettingsList from './components/SettingsList';
 
 const App = () => (
     <div id="app">
         <Navbar />
         <Router>{/* <Profile path="/profile/:user" /> */}</Router>
+        <Router>
+            <SettingsList path="/" />
+        </Router>
     </div>
 );
 
---
+++ src/components/SettingsList.jsx
@@ -0,0 +1,29 @@
+import { h } from 'preact';
+import List from 'preact-material-components/List';
+import { route } from 'preact-router';
+
+const settingItems = [
+    { text: 'Change Time', link: 'change-time' },
+    { text: 'Change Date', link: 'change-date' },
+    { text: 'Change Password', link: 'change-pass' },
+    { text: 'Edit Main Header', link: 'edit-main' },
+    { text: 'Edit Footer', link: 'edit-footer' },
+    { text: 'Change Paper cutting', link: 'change-cutting' },
+    { text: 'Set first token number', link: 'set-first' },
+];
+
+const SettingsList = () => {
+    return (
+        <div>
+            <List>
+                {settingItems.map(item => (
+                    <List.Item onClick={e => route(`/${item.link}`)}>
+                        {item.text}
+                    </List.Item>
+                ))}
+            </List>
+        </div>
+    );
+};
+
+export default SettingsList;
